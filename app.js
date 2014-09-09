f896fd1dec3c562f83ae873670c6a1db5cccf268
Andy <andrew.davies@matchtechgroupplc.com>
2014-09-09 11:03:37 +0100

    app: open files based on url fragment

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+
 var markdown = require("markdown").markdown;
 
-var file = fs.readFileSync('d:/dev/notes/tech-talks.md');
+var config = {
+	port: 1080,
+	fileRoot: 'd:/dev/notes'
+};
 
 http.createServer(function (req, res) {
-	res.writeHeader(200, {"Content-Type": "text/html"});
-	res.write(markdown.toHTML(file.toString()));
-	res.end();
-}).listen(10304);
+
+	var filePath = path.join(config.fileRoot, req.url);
+
+	fs.readFile(filePath, function(err, data) {
+
+		if (err) {
+
+			res.writeHeader(404, {"Content-Type": "text/html"});
+			res.write("Cannot find file '" + req.url + "'");
+
+		} else {
+
+			res.writeHeader(200, {"Content-Type": "text/html"});
+			res.write(markdown.toHTML(data.toString()));
+
+		}
+
+		res.end();
+
+	});
+
+}).listen(config.port);
+
+console.log("Listening on port " + config.port + ".");
