d1852c3294a3ece027df9314cae55b455e3e34d7
Andy <andrew.davies@matchtechgroupplc.com>
2014-09-09 14:32:55 +0100

    app: add index page listing all files

--- app.js
+++ app.js
@@ -2,23 +2,55 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+var router = require('router')();
 var markdown = require("markdown").markdown;
+var walk    = require('walk');
 
 var config = {
 	port: 1080,
 	fileRoot: 'd:/dev/notes'
 };
 
-http.createServer(function (req, res) {
+router.get('/', function(req, res) {
 
-	var filePath = path.join(config.fileRoot, req.url);
+	var files = [];
+	var walker = walk.walk(config.fileRoot, { followLinks: false });
+
+	walker.on('file', function(root, stat, next) {
+
+		var completePath = root + '/' + stat.name;
+
+		files.push(completePath.replace(config.fileRoot, ''));
+		next();
+
+	});
+
+	walker.on('end', function() {
+
+		res.writeHeader(200, {"Content-Type": "text/html"});
+
+		res.write("<ul>\r\n");
+
+		files.forEach(function(file) {
+			res.write("<li><a href='files" + file + "'>" + file + "</a></li>\r\n");
+		});
+
+		res.write("</ul>");
+		res.end();
+	});
+
+});
+
+router.get('/files/*', function (req, res) {
+
+	var filePath = path.join(config.fileRoot, req.params.wildcard);
 
 	fs.readFile(filePath, function(err, data) {
 
 		if (err) {
 
 			res.writeHeader(404, {"Content-Type": "text/html"});
-			res.write("Cannot find file '" + req.url + "'");
+			res.write("Cannot find file '" + req.params.wildcard + "'");
 
 		} else {
 
@@ -31,6 +63,8 @@ http.createServer(function (req, res) {
 
 	});
 
-}).listen(config.port);
+});
+
+http.createServer(router).listen(config.port);
 
 console.log("Listening on port " + config.port + ".");
